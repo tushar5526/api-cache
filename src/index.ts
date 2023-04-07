9338878e1a9abebf8a9d89b7e661c87f7ec8594c
Tushar Gupta <codingid6@gmail.com>
2023-04-08 00:15:00 +0530

    add cache invalidation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-import { PrismaClient, Song } from '@prisma/client'
+import { PrismaClient, Song, Artist } from '@prisma/client'
 import express from 'express'
 import Redis from 'ioredis'
 import { RedisCache } from 'layered-loader/dist/lib/redis'
@@ -16,7 +16,7 @@ const ioRedis = new Redis({
     password: 'sOmE_sEcUrE_pAsS',
 })
 
-class ClassifiersLoader implements Loader<string> {
+class ClassifiersLoader implements Loader<Artist[] | Song | Song[]> {
     name = 'Classifiers DB loader'
     isCache = false
     private readonly prismaClient: PrismaClient
@@ -25,15 +25,32 @@ class ClassifiersLoader implements Loader<string> {
         this.prismaClient = prismaClient
     }
 
-    async get(key: string): Promise<string | undefined | null> {
-        const song = await this.prismaClient.song.findFirst({
-            where: { id: Number(key) },
-        })
-        return song?.title;
+    async get(key: string): Promise<Artist[] | Song | Song[] | undefined | null> {
+        /*
+        song_<id>
+        playlist
+        artists
+        */
+        if (key === 'artists')
+            return await this.prismaClient.artist.findMany()
+        if (key === 'playlist')
+            return await prisma.song.findMany({
+                where: { released: true },
+                include: { singer: true }
+            })
+        if (key.startsWith('song_'))
+        {
+            const song = await this.prismaClient.song.findFirst({
+                where: { id: Number(key.split('_')[1]) },
+            })
+            console.log(song)
+            return song;
+        }
+        return undefined
     }
 }
 
-const operation = new LoadingOperation<string>({
+const operation = new LoadingOperation<Artist[] | Song | Song[]>({
     // this cache will be checked first
     inMemoryCache: {
         ttlInMsecs: 1000 * 60,
@@ -50,14 +67,12 @@ const operation = new LoadingOperation<string>({
     loaders: [new ClassifiersLoader(prisma)]
 })
 
+
 // If cache is empty, but there is data in the DB, after this operation is completed, both caches will be populated
 
 //* 1. Fetches all released songs.
 app.get('/playlist', async (req, res) => {
-    const songs = await prisma.song.findMany({
-        where: { released: true },
-        include: { singer: true }
-    })
+    const songs = await operation.get('playlist')
     res.json({
         success: true,
         payload: songs,
@@ -67,11 +82,19 @@ app.get('/playlist', async (req, res) => {
 //* 2. Fetches a specific song by its ID.
 app.get(`/song/:id`, async (req, res) => {
     const { id } = req.params
-    const classifier = await operation.get(id)
-    console.log(classifier)
+    let song = await operation.get('song_' + id)
     res.json({
         success: true,
-        payload: classifier,
+        payload: song,
+    })
+})
+
+//* 7. Fetches all Artist.
+app.get('/artists', async (req, res) => {
+    const artists = await operation.get('artists')
+    res.json({
+        success: true,
+        payload: artists,
     })
 })
 
@@ -110,6 +133,8 @@ app.put('/song/release/:id', async (req, res) => {
         where: { id: Number(id) },
         data: { released: true },
     })
+    await operation.invalidateCacheFor('playlist')
+    await operation.invalidateCacheFor('song_' + id)
     res.json({
         success: true,
         payload: song,
@@ -122,20 +147,15 @@ app.delete(`/song/:id`, async (req, res) => {
     const song = await prisma.song.delete({
         where: { id: Number(id) },
     })
+    await operation.invalidateCacheFor('song_' + id)
+    if(song.released) await operation.invalidateCacheFor('playlist')
     res.json({
         success: true,
         payload: song,
     })
 })
 
-//* 7. Fetches all Artist.
-app.get('/artists', async (req, res) => {
-    const artists = await prisma.artist.findMany()
-    res.json({
-        success: true,
-        payload: artists,
-    })
-})
+
 
 app.use((req, res, next) => {
     res.status(404);
